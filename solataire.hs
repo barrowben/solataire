4c682cfb2df6c900e55a3177f9285f67c7d32417
Ben Barrow <babarrow1@sheffield.ac.uk>
2021-11-02 18:44:52 +0000

    Implemented Deal function

--- solataire.hs
+++ solataire.hs
@@ -5,20 +5,19 @@ data Pip = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | 
 data Suit = Clubs | Diamonds | Hearts | Spades deriving (Eq, Enum, Show, Bounded)
 data Card = Card Pip Suit
 instance Show Card where
-    show (Card p s) = "(" ++ show p ++ "," ++ show s ++ ")"
+    show (Card p s) = "(" ++ show p ++ " of " ++ show s ++ ")"
 
 type Deck = [Card]
+type Foundation = [Card]
+type Reserve = [Card]
+type Column = [Card]
 
-type Foundation = [Deck]
-type Reserve = Deck
-type Column = Deck
+data Board = EOBoard
+         [Foundation]
+         [Column]
+         Reserve
 
-data EOBoard = EOBoard
-         Foundation  -- foundations
-         [Column]  -- columns
-         Reserve   -- reserve
-
-instance Show EOBoard where
+instance Show Board where
     show (EOBoard f cs r) =
         "EOBoard\n" ++
         "Foundations  " ++ show f ++
@@ -30,36 +29,11 @@ instance Show EOBoard where
         show (cs!!4) ++ "\n" ++
         show (cs!!5) ++ "\n" ++
         show (cs!!6) ++ "\n" ++
-        show (cs!!7) ++ "\n" ++                
+        show (cs!!7) ++ "\n" ++
         "\nReserve  " ++ show r
 
 pack :: Deck
 pack = [Card pip suit | pip <- [Ace .. King], suit <- [Clubs .. Spades]]
-
-fourCards :: Deck
-fourCards = [Card Three Hearts, Card Two Clubs, Card Seven Diamonds, Card King Clubs]
-
-sixCards :: Deck
-sixCards = [Card Ace Clubs, Card Two Hearts, Card Three Diamonds, Card Ace Hearts, Card Six Hearts, Card Seven Diamonds]
-
-shuffledPack :: Deck
-shuffledPack = shuffle 1234 pack
-
-testFound :: Foundation
-testFound = []
-
-testCol :: Column
-testCol = sixCards
-
-testReserve :: Reserve
-testReserve = fourCards
-
-testEO :: EOBoard
-testEO = EOBoard testFound [testCol, testCol, testCol, testCol, testCol, testCol, testCol, testCol] testReserve
-
--- test cards
-tCard = Card King Clubs
-tCard' = Card Ace Hearts
 
 sCard :: Card -> Card
 sCard (Card p s)
@@ -72,16 +46,34 @@ pCard (Card p s)
     | otherwise = Card (pred p) s
 
 isAce :: Card -> Bool
-isAce (Card p _) | p == Ace = True 
+isAce (Card p _) | p == Ace = True
                  | otherwise = False
 
 isKing :: Card -> Bool
-isKing (Card p _) | p == King = True 
+isKing (Card p _) | p == King = True
                   | otherwise = False
 
 -- Shuffle deck of cards
-cmp (x1,y1) (x2,y2) = compare y1 y2
+cmp (x1, y1) (x2, y2) = compare y1 y2
 shuffle :: Int -> [a] -> [a]
 shuffle n xs = [ x | (x, n) <- sortBy cmp
-                          (zip xs (randoms (mkStdGen n) :: [Int]))]
+                          (zip xs (randoms (mkStdGen n) :: [Int]))]
 
+-- Takes seed and generates random starting setup
+eODeal :: Int -> Board
+eODeal seed = EOBoard fnds cols res
+    where
+        shuffled = shuffle seed pack
+        fnds  = [[],[],[],[]]
+        cols  = [take 6 shuffled,
+                take 6 (drop 6 shuffled),
+                take 6 (drop 12 shuffled),
+                take 6 (drop 18 shuffled),
+                take 6 (drop 24 shuffled),
+                take 6 (drop 30 shuffled),
+                take 6 (drop 36 shuffled),
+                take 6 (drop 42 shuffled)]
+        res   = [shuffled!!48,
+                shuffled!!49,
+                shuffled!!50,
+                shuffled!!51]
