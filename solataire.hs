558aebef48e6e50de31c8ce6204d098b5f5aceb9
Benedict Barrow <benedictbarrow@Benedicts-MacBook-Pro.local>
2021-11-02 17:07:52 +0000

    implemented up to end of step 3

--- solataire.hs
+++ solataire.hs
@@ -3,16 +3,59 @@ import Data.List
 
 data Pip = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King deriving (Eq, Enum, Show, Bounded)
 data Suit = Clubs | Diamonds | Hearts | Spades deriving (Eq, Enum, Show, Bounded)
-data Card = Card Pip Suit deriving (Show)
+data Card = Card Pip Suit
+instance Show Card where
+    show (Card p s) = "(" ++ show p ++ "," ++ show s ++ ")"
+
 type Deck = [Card]
 
-newtype Foundation = Foundation Deck
-newtype Column =  Column Deck
-newtype Reserve = Reserve Deck
-newtype EOBoard = EOBoard Deck
+type Foundation = [Deck]
+type Reserve = Deck
+type Column = Deck
+
+data EOBoard = EOBoard
+         Foundation  -- foundations
+         [Column]  -- columns
+         Reserve   -- reserve
+
+instance Show EOBoard where
+    show (EOBoard f cs r) =
+        "EOBoard\n" ++
+        "Foundations  " ++ show f ++
+        "\nColumns\n" ++
+        show (head cs) ++ "\n" ++
+        show (cs!!1) ++ "\n" ++
+        show (cs!!2) ++ "\n" ++
+        show (cs!!3) ++ "\n" ++
+        show (cs!!4) ++ "\n" ++
+        show (cs!!5) ++ "\n" ++
+        show (cs!!6) ++ "\n" ++
+        show (cs!!7) ++ "\n" ++                
+        "\nReserve  " ++ show r
 
 pack :: Deck
 pack = [Card pip suit | pip <- [Ace .. King], suit <- [Clubs .. Spades]]
+
+fourCards :: Deck
+fourCards = [Card Three Hearts, Card Two Clubs, Card Seven Diamonds, Card King Clubs]
+
+sixCards :: Deck
+sixCards = [Card Ace Clubs, Card Two Hearts, Card Three Diamonds, Card Ace Hearts, Card Six Hearts, Card Seven Diamonds]
+
+shuffledPack :: Deck
+shuffledPack = shuffle 1234 pack
+
+testFound :: Foundation
+testFound = []
+
+testCol :: Column
+testCol = sixCards
+
+testReserve :: Reserve
+testReserve = fourCards
+
+testEO :: EOBoard
+testEO = EOBoard testFound [testCol, testCol, testCol, testCol, testCol, testCol, testCol, testCol] testReserve
 
 -- test cards
 tCard = Card King Clubs
