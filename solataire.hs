f7f1cfb10b52806aa18c9c7045d7e695d9955153
Ben Barrow <babarrow1@sheffield.ac.uk>
2021-11-03 20:15:08 +0000

    commit before starting on spider

--- solataire.hs
+++ solataire.hs
@@ -4,6 +4,10 @@ solataire.hs
 8-off solataire game
 https://en.wikipedia.org/wiki/Eight_Off
 
+NOTE: For the columns, the last card in the list represents the bottommost card
+i.e. the ones which can potentially be moved. Any card at the start of the list
+cannot be moved until its successor are moved.
+
 Author: Ben Barrow
 Date: 03.11.2021
 -}
@@ -26,26 +30,24 @@ type Deck = [Card]
 type Foundation = [Card]
 type Reserve = [Card]
 type Column = [Card]
+type Stock = [Card]
 
-data Board = EOBoard
-         [Foundation]
-         [Column]
-         Reserve
+data Board = EOBoard [Foundation] [Column] Reserve
 
 instance Show Board where
     show (EOBoard f cs r) =
-        "EOBoard\n" ++
-        "Foundations  " ++ show f ++
+        "\nEOBoard\n" ++
+        "Foundations   " ++ show f ++
         "\nColumns\n" ++
-        show (head cs) ++ "\n" ++
-        show (cs!!1) ++ "\n" ++
-        show (cs!!2) ++ "\n" ++
-        show (cs!!3) ++ "\n" ++
-        show (cs!!4) ++ "\n" ++
-        show (cs!!5) ++ "\n" ++
-        show (cs!!6) ++ "\n" ++
-        show (cs!!7) ++ "\n" ++
-        "\nReserve  " ++ show r
+        "  " ++ show (head cs) ++ "\n" ++
+        "  " ++ show (cs!!1) ++ "\n" ++
+        "  " ++ show (cs!!2) ++ "\n" ++
+        "  " ++ show (cs!!3) ++ "\n" ++
+        "  " ++ show (cs!!4) ++ "\n" ++
+        "  " ++ show (cs!!5) ++ "\n" ++
+        "  " ++ show (cs!!6) ++ "\n" ++
+        "  " ++ show (cs!!7) ++ "\n" ++
+        "Reserve   " ++ show r ++ "\n"
 
 -- Define a 52-card deck
 pack :: Deck
@@ -76,27 +78,26 @@ isKing (Card p _) | p == King = True
 -- Shuffle deck of cards
 cmp (x1, y1) (x2, y2) = compare y1 y2
 shuffle :: Int -> [a] -> [a]
-shuffle n xs = [ x | (x, n) <- sortBy cmp
-                          (zip xs (randoms (mkStdGen n) :: [Int]))]
+shuffle n xs = [ x | (x, n) <- sortBy cmp (zip xs (randoms (mkStdGen n) :: [Int]))]
 
 -- Generate 8 columns of 6 cards
-startColumns :: Deck -> [Column]
-startColumns d = [take 6 d,
-                 take 6 (drop 6 d),
-                 take 6 (drop 12 d),
-                 take 6 (drop 18 d),
-                 take 6 (drop 24 d),
-                 take 6 (drop 30 d),
-                 take 6 (drop 36 d),
-                 take 6 (drop 42 d)]
+startCol :: Deck -> [Column]
+startCol d = [take 6 d,
+                take 6 (drop 6 d),
+                take 6 (drop 12 d),
+                take 6 (drop 18 d),
+                take 6 (drop 24 d),
+                take 6 (drop 30 d),
+                take 6 (drop 36 d),
+                take 6 (drop 42 d)]
 
 -- Initial board setup
 eODeal :: Int -> Board
-eODeal seed = EOBoard foundations columns reserve
+eODeal rand = EOBoard foundations columns reserve
     where
-        shuffled = shuffle seed pack
+        shuffled = shuffle rand pack
         foundations  = [[],[],[],[]]
-        columns = startColumns shuffled
+        columns = startCol shuffled
         reserve = [shuffled!!48, shuffled!!49, shuffled!!50, shuffled!!51]
 
 -- Checks suit and returns an Int indicating to which Foundation pile it belongs
@@ -108,6 +109,7 @@ checkSuit (Card _ s)
     | otherwise = 3
 
 -- Add any card to correct pile in Foundation
+-- TODO: BIT FUNKY... REFACTOR?
 addCardFnd :: [Foundation] -> Card -> [Foundation]
 addCardFnd f c
     | checkSuit c == 0 = [c]:tail f -- Clubs
@@ -120,21 +122,28 @@ getBtmColCards :: [Column] -> [Card]
 getBtmColCards = map last
 
 -- Check bottom card in columns for passed in card and remove if present
+-- [DONE] Refactor using getBtmCards
+-- TODO: REMOVE WHEN VERIFIED NEW FUNCTION IS WORKING CORRECTLY
+-- removeFromCol :: [Column] -> Card -> [Column]
+-- removeFromCol col card
+--     | card == last (head col) = head (init col):tail col
+--     | card == last (col!!1) = head col:init (col!!1):drop 2 col
+--     | card == last (col!!2) = head col:col!!1:init (col!!2):drop 3 col
+--     | card == last (col!!3) = head col:col!!1:col!!2:init (col!!3):drop 4 col
+--     | card == last (col!!4) = head col:col!!1:col!!2:col!!3:init (col!!4):drop 5 col
+--     | card == last (col!!5) = head col:col!!1:col!!2:col!!3:col!!4:init (col!!5):drop 6 col
+--     | card == last (col!!6) = head col:col!!1:col!!2:col!!3:col!!4:col!!5:init (col!!6):drop 7 col
+--     | card == last (col!!7) = head col:col!!1:col!!2:col!!3:col!!4:col!!5:col!!6:init (col!!7):drop 8 col -- This bit at the end is black magic, ask someone to explain why it behaves this this
+--     | otherwise = col
+
 removeFromCol :: [Column] -> Card -> [Column]
-removeFromCol col car
-    | car == last (head col) = head (init col):tail col
-    | car == last (col!!1) = head col:init (col!!1):drop 2 col
-    | car == last (col!!2) = head col:col!!1:init (col!!2):drop 3 col
-    | car == last (col!!3) = head col:col!!1:col!!2:init (col!!3):drop 4 col
-    | car == last (col!!4) = head col:col!!1:col!!2:col!!3:init (col!!4):drop 5 col
-    | car == last (col!!5) = head col:col!!1:col!!2:col!!3:col!!4:init (col!!5):drop 6 col
-    | car == last (col!!6) = head col:col!!1:col!!2:col!!3:col!!4:col!!5:init (col!!6):drop 7 col
-    | car == last (col!!7) = head col:col!!1:col!!2:col!!3:col!!4:col!!5:col!!6:init (col!!7):drop 8 col
+removeFromCol col card 
+    | card `elem` getBtmColCards col = map (delete card) col
     | otherwise = col
 
 removeFromReserve :: Reserve -> Card -> Reserve
-removeFromReserve res car
-    | car `elem` res = filter (/=car) res
+removeFromReserve res card
+    | card `elem` res = filter (/=card) res
     | otherwise = res
 
 -- Moves all legal cards to Foundations
