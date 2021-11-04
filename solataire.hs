a2f3ef842c7cc271c89e3ef262de615a86da2cbb
Ben Barrow <babarrow1@sheffield.ac.uk>
2021-11-04 01:21:23 +0000

    changed show to hide facedown cards

--- solataire.hs
+++ solataire.hs
@@ -4,7 +4,7 @@ solataire.hs
 8-off solataire game
 https://en.wikipedia.org/wiki/Eight_Off
 
-NOTE: For the columns, the last card in the list represents the bottommost card
+NOTE: For the columns, the last card in the list represents the bottom-most card
 i.e. the ones which can potentially be moved. Any card at the start of the list
 cannot be moved until its successor are moved.
 
@@ -13,15 +13,9 @@ cannot be moved until its successor are moved.
 A constant of type Board that shows the game in progress (that is, the state of the board at
 that particular move) for the screenshot shown in Appendix B.
 + type vs data?
-+ How to show face-up/face down cards - Card or Column?
-
-
-TODO:
-* Add remaining deals counter
-* 
 
 Author: Ben Barrow
-Date: 03.11.2021
+Date: 04.11.2021
 -}
 
 import System.Random
@@ -36,7 +30,7 @@ data Card = Card {pip :: Pip, suit :: Suit, faceup :: Bool}
             deriving (Eq)
 
 instance Show Card where
-    show (Card p s u) = "(" ++ show p ++ " of " ++ show s ++ ")" ++ "[" ++ show u ++ "]"
+    show (Card p s u) = if u then "(" ++ show p ++ " of " ++ show s ++ ")" else "<unknown>"
 
 type Deck = [Card]
 type Foundation = [Card]
@@ -78,9 +72,9 @@ instance Show Board where
         "  " ++ show (cs!!9) ++ "\n" ++
         "Stock " ++ show (length r `div` 10) ++ " Deals Remaining "
 
--- Define a 52-card deck
+-- Define a 52-card deck, cards face-up by default
 pack :: Deck
-pack = [Card p s u | p <- [minBound .. maxBound], s <- [minBound .. maxBound], u <- [True ..] ]
+pack = [Card p s u | p <- [minBound .. maxBound], s <- [minBound .. maxBound], u <- [True ..]]
 
 -- Define a 104-card deck
 sPack :: Deck
@@ -92,7 +86,7 @@ sCard (Card p s _)
     | p == King = Card Ace s True
     | otherwise = Card (succ p) s True
 
--- Get predecessor card
+-- Get predecessor card [NOT USED]
 pCard :: Card -> Card
 pCard (Card p s _)
     | p == Ace = Card King s True
@@ -103,7 +97,7 @@ isAce :: Card -> Bool
 isAce (Card p _ _) | p == Ace = True
                  | otherwise = False
 
--- Determine if King
+-- Determine if King [NOT USED]
 isKing :: Card -> Bool
 isKing (Card p _ _) | p == King = True
                   | otherwise = False
@@ -113,7 +107,7 @@ cmp (x1, y1) (x2, y2) = compare y1 y2
 shuffle :: Int -> [a] -> [a]
 shuffle n xs = [ x | (x, n) <- sortBy cmp (zip xs (randoms (mkStdGen n) :: [Int]))]
 
--- Generate 8 columns of 6 cards
+-- Generate starting columns for eight-off
 startCol :: Deck -> [Column]
 startCol d = [take 6 d,
                 take 6 (drop 6 d),
@@ -124,7 +118,7 @@ startCol d = [take 6 d,
                 take 6 (drop 36 d),
                 take 6 (drop 42 d)]
 
--- Generate 8 columns of 6 cards
+-- Generate starting columns for spider
 sStartCol :: Deck -> [Column]
 sStartCol d = [take 6 d,
                 take 6 (drop 6 d),
@@ -146,9 +140,9 @@ eODeal rand = EOBoard foundations columns reserve
         columns = startCol shuffled
         reserve = [shuffled!!48, shuffled!!49, shuffled!!50, shuffled!!51]
 
--- Initial spider setup
+-- Initial spider board setup
 sDeal :: Int -> Board
-sDeal rand = hideAllCol (SBoard f c s)
+sDeal rand = hideAll (SBoard f c s)
     where
         shuffled = shuffle rand sPack
         f = [[],[],[],[],[],[],[],[]]
@@ -156,16 +150,15 @@ sDeal rand = hideAllCol (SBoard f c s)
         s = drop 54 shuffled
 
 -- Functions to hide cards
-hideAllCol :: Board -> Board
-hideAllCol (SBoard f c s) = SBoard f (map hideCards c) s
+hideAll :: Board -> Board
+hideAll (SBoard f c s) = SBoard f (map hideCards c) s
 
 hideCards :: Column -> Column
 hideCards col = map flipCard facedown ++ [last col]
     where facedown = init col
 
 flipCard :: Card -> Card
-flipCard card = card
-    { faceup = False }
+flipCard card = card { faceup = False }
 
 -- Checks suit and returns an Int indicating to which Foundation pile it belongs
 checkSuit :: Card -> Int
